cc1ec4962270f02ee205f7b27c0733b5ddb140ea
Aryan Gadhwal <gadhwalaryan1@gmail.com>
2025-08-31 12:28:21 +0530

    Add files via upload

---
+++ count digits.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int digitCount(int n) {
+    int cnt = 0;
+    while (n > 0) {
+        int lastdigit = n % 10;
+        cnt++;
+        n = n / 10;
+    }
+    return cnt;
+}
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    int result = digitCount(n);
+    cout << "Number of digits: " << result << endl;
+    return 0;
+}
