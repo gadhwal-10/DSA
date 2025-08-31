1a140d7e8f36e94717d399663216cbe7a3551968
Aryan Gadhwal <gadhwalaryan1@gmail.com>
2025-08-31 12:38:40 +0530

    counting number of digits

--- count digits.cpp
+++ count digits.cpp
@@ -4,7 +4,6 @@ using namespace std;
 int digitCount(int n) {
     int cnt = 0;
     while (n > 0) {
-        int lastdigit = n % 10;
         cnt++;
         n = n / 10;
     }
@@ -20,4 +19,5 @@ int main() {
     return 0;
     
 }
+
 
