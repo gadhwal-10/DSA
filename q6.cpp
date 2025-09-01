fece647d8e40d5705563ce98e9d68c703f4b8cfc
Aryan Gadhwal <gadhwalaryan1@gmail.com>
2025-09-01 23:03:11 +0530

    prime number checking

---
+++ q6.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int x;
+	cout<<"Enter a number ";
+	cin>>x;
+    int cnt=0;
+	for(int i=1;i*i<=x ;i++){
+		
+		if(x%i==0){
+			cnt++;
+		if(x/i!=i){
+			cnt++;
+		}
+}
+	}
+	
+	if (cnt==2){
+		cout<<"prime";
+	}
+	else{
+		cout<<"not a prime";
+	}
+	
+	return 0;
+}
+
+
+
+
+
+
+
+
+
