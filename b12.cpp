44a7fc8314dc083bf0d49f7e1a1e53dbfc7025e8
Aryan Gadhwal <gadhwalaryan1@gmail.com>
2025-09-05 15:52:27 +0530

    Rotate array d places (leetcode 189)

---
+++ b12.cpp
@@ -0,0 +1,37 @@
+//Time Complexity - O(2n)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, k;
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    vector<int> nums(n);
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    cout << "Enter no. of place to reverse: ";
+    cin >> k;
+
+    k = k % n;  
+
+    reverse(nums.begin(), nums.end());
+
+ 
+    reverse(nums.begin(), nums.begin() + k);
+
+ 
+    reverse(nums.begin() + k, nums.end());
+
+    cout << "Rotated array: ";
+    for (int x : nums) {
+        cout << x << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
