b7fd2e49deaec5591182ea81078f89ba7990f9f3
Aryan Gadhwal <gadhwalaryan1@gmail.com>
2025-09-06 22:08:02 +0530

    Missing Number (leetcode 268 easy)

---
+++ b15.cpp
@@ -0,0 +1,16 @@
+//Time Complexity - O(n)
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int totalSum = (n * (n + 1)) / 2;
+        int arraySum = 0;
+
+        for (int num : nums) {
+            arraySum += num;
+        }
+
+        return totalSum - arraySum;
+    }
+};
